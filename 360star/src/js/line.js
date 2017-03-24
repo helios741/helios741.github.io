49df61465519a6ce6ce6cb54a3d97d54e1fb1d7f
nannan <674876158@qq.com>
2017-03-24 22:40:42 +0800

    addEventLinstener出现this的问题

--- 360star/src/js/line.js
+++ 360star/src/js/line.js
@@ -1,3 +1,66 @@
 ;(function(){
-	
+	function line(w,h,item) {
+		var itemArr = [];
+		for (var i = 0; i < item.length; i++) {
+			itemArr.push({
+				left: item[i].offsetLeft,
+				top: item[i].offsetTop
+			});
+		}
+		this.x = 0;
+		this.y = 0;
+		this.w = w;
+		this.h = h;
+		this.item = item;
+		this.itemW = 30;
+		this.itemH = 30;
+		this.arr = itemArr;
+		this.len = itemArr.length;
+		this.isDone = true;
+		this.color = 'rgb(224,43,27)';
+	};
+	line.prototype = {
+		constructor: line,
+		touchStart: function(e) {
+			// line.call(this);
+			console.log(this);
+			e.preventDefault();
+			var touch = e.touches[0] || {}, 
+			    x = touch.clientX || e.clientX,
+			    y = touch.clientY || e.clientY;
+			console.log(x,y); console.log("---",this.len);
+			for (var i = 0; i < this.len; i++) {
+				console.log(arr.left,arr.top);
+				if( x < arr.left || x > arr.left+this.itemW) {
+					continue;
+				} else if(y < arr.top || y > arr.top + this.itemH) {
+					continue;
+				}
+				Class.addClass(item[i],'active');
+				this.x = x,
+				this.y = y;
+				this.isDone = false;
+			}
+
+		},
+		touchMove: function(e,cobj) {
+			if(this.isDone) return ;
+			e.preventDefault();
+			var touch = e.touches[0];
+			cobj.clearRect(0,0,this.w,this.h);
+			cobj.lineWidth = 6;
+			cobj.beginPath();
+			cobj.moveTo(this.x, this.y);
+			cobj.lineTo(touch.pageX,touch.pageY);
+			cobj.stroke();
+		},
+		touchEnd: function(e){
+			this.isDone = true;
+			e.preventDefault();
+			cobj.stroke();
+			cobj.closePath();
+			cobj.clearRect(0,0,this.w,this.h);
+		}
+	};
+	window.Line = line;
 })()
--- 360star/src/js/main.js
+++ 360star/src/js/main.js
@@ -1,8 +1,15 @@
 var gesPWD = document.getElementById('gesPWD'),
+	item = gesPWD.getElementsByTagName('span'),
 	msg = document.getElementById('msg'),
 	aboutPWD = document.getElementById('aboutPWD'),
+	canvas  = document.getElementById('canvas'),
 	radio = aboutPWD.getElementsByTagName('input'),
-	radioLen = radio.length;
+	radioLen = radio.length,
+	itemLen = item.length,
+	gesPWDWidth = gesPWD.offsetWidth,
+	gesPWDHeight = gesPWD.offsetHeight,
+	line = new Line(gesPWDWidth, gesPWDHeight, item),
+	cobj = canvas.getContext('2d');
 
 
 function checkOpt(event) {
@@ -19,17 +26,9 @@ function checkOpt(event) {
 		msg.innerHTML = "请输入您的密码";
 	}
 }
-function chooseItem(event) {
-	var target = Event.getTarget(event);
-	if(target.nodeName.toLowerCase() !== 'span') {
-		return ;
-	}
-	Class.addClass(target,'active');
-	//target.style.background = 'rgba(255,177,38,.5)';
-}
+
 Event.addEvent(aboutPWD,checkOpt,'touchstart');
 Event.addEvent(aboutPWD,checkOpt,'click');
-
-Event.addEvent(gesPWD,chooseItem,'touchstart');
-// Event.addEvent(gesPWD,chooseItem,'click');
+Event.addEvent(canvas,line.touchStart,'touchstart');
+Event.addEvent(canvas,line.touchStart,'click');
 
