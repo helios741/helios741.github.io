5ac93c99d6d029fea73abdedd314e811bba00ebf
nannan <674876158@qq.com>
2017-06-06 21:33:40 +0800

    卡在了检测生成的大奇数是不是素数

--- rsa/app/Prim/index.js
+++ rsa/app/Prim/index.js
@@ -1,8 +1,26 @@
 import primBase from './primBase';
-
+import BigInt from '../bigInt';
+const PRIMBYTESUM = 1e20;
 export default class Prim extends primBase{
 	constructor(mx) {
 		super(mx);
+		this.bigInt = new BigInt();
+	}
+	createBigOddPrim() {
+		let oddNum = Math.random() * PRIMBYTESUM;
+		while(oddNum%2) oddNum = Math.random() * PRIMBYTESUM;
+		return oddNum;
+	}
+	preTestPrim() {
+		let oddPrim = createBigOddPrim();
+		const prim = this.prim,
+			bigInt = this.bigInt,
+			num = this.num;
+		for (let i = 0; i < num; i++) {
+			if( bigInt.mod(oddPrim,prim[i]) ) continue;
+			return this.preTestPrim();
+		}
+		return oddPrim;
 	}
 	
 }
--- rsa/app/index.js
+++ rsa/app/index.js
@@ -1,7 +1,7 @@
 import BigInt from './bigInt';
 import Prim from './Prim';
 let big = new BigInt(0,2);
-let prim = new Prim(2000);
+let prim = new Prim(800);
 
 
 console.log( big.mod(55555555555,5) );
